601cd4ff81915a0d12da16277220f9152b322d84
Richard <richard@besitegroup.nl>
2013-02-25 12:30:25 +0100

    Optimize PNG

--- web/thumb.php
+++ web/thumb.php
@@ -1,3 +1,4 @@
 <?php
-define ('FILE_CACHE_DIRECTORY', '../vendor/bobdenotter/bolt/app/cache/thumbs/');
+define('OPTIPNG_ENABLED', true);
+define('FILE_CACHE_DIRECTORY', '../vendor/bobdenotter/bolt/app/cache/thumbs/');
 require_once( 'bolt-public/classes/timthumb.php' );
