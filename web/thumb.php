5760aa115e133e4787906ed201abaee4add4c2ee
Richard <richard@besitegroup.nl>
2013-04-02 15:35:38 +0200

    Fix new bolt dir

--- web/thumb.php
+++ web/thumb.php
@@ -1,4 +1,4 @@
 <?php
 define('OPTIPNG_ENABLED', true);
-define('FILE_CACHE_DIRECTORY', '../vendor/bobdenotter/bolt/app/cache/thumbs/');
+define('FILE_CACHE_DIRECTORY', '../vendor/bolt/bolt/app/cache/thumbs/');
 require_once( 'bolt-public/classes/timthumb.php' );
