36ff02635db72e85d833fe34cb5fbc6aa18d8932
Richard Hinkamp <richard@hinkamp.nl>
2014-02-27 20:10:15 +0100

    Latest bolt, sitemap fixes

--- web/timthumb.php
+++ web/timthumb.php
@@ -1,4 +1,5 @@
 <?php
 define('OPTIPNG_ENABLED', true);
-define('FILE_CACHE_DIRECTORY', '../vendor/bolt/bolt/app/cache/thumbs/');
+define('FILE_CACHE_DIRECTORY', '../cache/thumbs/');
+require_once( '../app/src/bootstrap.php' );
 require_once( 'bolt-public/classes/timthumb.php' );
