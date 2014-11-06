fd5763e8f26c21e145aef620a3929d826914cfde
Richard Hinkamp <richard@hinkamp.nl>
2014-11-06 23:28:07 +0100

    Not needed anymore

--- web/timthumb.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('OPTIPNG_ENABLED', true);
-define('FILE_CACHE_DIRECTORY', '../cache/thumbs/');
-require_once( '../app/src/bootstrap.php' );
-require_once( 'bolt-public/classes/timthumb.php' );
