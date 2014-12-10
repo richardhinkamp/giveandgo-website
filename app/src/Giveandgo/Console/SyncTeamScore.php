f59eb89bd3ebeb86c6394cbf4b588934bcb5bc08
Richard Hinkamp <richard@hinkamp.nl>
2014-12-10 11:10:26 +0100

    Change sync script to use JSON API instead of parsing HTML

--- app/src/Giveandgo/Console/SyncTeamScore.php
+++ app/src/Giveandgo/Console/SyncTeamScore.php
@@ -25,61 +25,40 @@ class SyncTeamScore extends BaseCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->guzzleclient = new Client('http://db.basketball.nl/db/wedstrijd/uitslag.pl');
+        $this->guzzleclient = new Client('http://db.basketball.nl/db/json/wedstrijd.pl');
         $teams = $this->app['storage']->getContent('teams',array());
         foreach($teams as $team) {
             if($team->get('nbb_team_id')>0 && $team->get('nbb_competitie_id')>0) {
-                //$output->writeln("<info>".$team->get('title')."</info>");
-                $this->doTeam($team->get('team_id'),$team->get('nbb_competitie_id'),$team->get('nbb_team_id'));
+                $this->doTeam($team->get('team_id'), $team->get('nbb_team_id'));
             }
         }
     }
 
-    protected function doTeam($teamId,$nbbCompId, $nbbTeamId) {
-        $html = $this->guzzleclient->get('?szn_Naam='.$this->app['season'].'&cmp_ID='.$nbbCompId.'&Sorteer=wed_Datum&LVactie=Wedstrijdgegevens&plg_ID='.$nbbTeamId.'&menubalk=off&title=off&link=off&statistieken=off&hal=off&nummer=off&warning=off')->send()->getBody(true);
-        if ($html) {
+    protected function doTeam($teamId, $nbbTeamId) {
+        $json = $this->guzzleclient->get('?szn_Naam='.$this->app['season'].'&plg_ID='.$nbbTeamId)->send()->getBody(true);
+        if ($json) {
             $scores = $this->app['storage']->getContent('scores',array('limit'=>9999),$p=array(),array('team_id'=>$teamId,'season'=>$this->app['season']));
-            $crawler = new Crawler($html);
-            $tdVals = $crawler->filter('table > tr > td')->extract(array('_text'));
             $baseGame = new \Bolt\Content($this->app,'scores',array());
             $baseGame->values['team_id'] = $teamId;
             $baseGame->values['season'] = $this->app['season'];
             $baseGame->values['slug'] = '';
             $baseGame->values['username'] = 'richard';
             $baseGame->values['status'] = 'published';
-            $game = clone($baseGame);
-            foreach($tdVals as $idx=>$val) {
-                if ($idx>0) { // contains all td text??
-                    switch($idx%5) {
-                        //case 1:
-                        //    $game['date'] = $val;
-                        //    break;
-                        case 2:
-                            $dt = new \DateTime($tdVals[$idx-1].' '.$val);
-                            $game->values['datetime'] = date('Y-m-d H:i:s',$dt->getTimestamp());
-                            break;
-                        case 3:
-                            $game->values['team_home'] = trim($val);
-                            break;
-                        case 4:
-                            $game->values['team_away'] = trim($val);
-                            break;
-                        case 0:
-                            if(preg_match('/^([0-9]+) \- ([0-9]+)/',$val,$m)) {
-                                $game->values['score_home'] = $m[1];
-                                $game->values['score_away'] = $m[2];
-                                foreach($scores as $idx => $score) {
-                                    if($score->get('datetime')==$game->values['datetime']) {
-                                        $game->values['id']=$score->id;
-                                        unset($scores[$idx]);
-                                    }
-                                }
-                                $this->app['storage']->saveContent($game);
-                                $game = clone($baseGame);
-                            }
-                            break;
+            $data = json_decode($json);
+            foreach($data->wedstrijden as $val) {
+                $game = clone($baseGame);
+                $game->values['datetime'] = date('Y-m-d H:i:s', strtotime($val->datum));
+                $game->values['team_home'] = $val->thuis_ploeg;
+                $game->values['team_away'] = $val->uit_ploeg;
+                $game->values['score_home'] = $val->score_thuis;
+                $game->values['score_away'] = $val->score_uit;
+                foreach($scores as $idx => $score) {
+                    if($score->get('datetime')==$game->values['datetime']) {
+                        $game->values['id']=$score->id;
+                        unset($scores[$idx]);
                     }
                 }
+                $this->app['storage']->saveContent($game);
             }
             foreach($scores as $score) {
                 $this->app['storage']->deleteContent('scores', $score->id);
