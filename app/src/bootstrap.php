6ab1425ba35d274606e487d28ec3edccd31f00e8
Richard Hinkamp <richard@hinkamp.nl>
2015-05-05 09:50:12 +0200

    Fix include path for zend

--- app/src/bootstrap.php
+++ app/src/bootstrap.php
@@ -6,6 +6,11 @@ if( is_dir(__DIR__ . '/../../public_html/') ) {
     define('BOLT_WEB_DIR', __DIR__ . '/../../web/');
 }
 
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(__DIR__ . '/../../vendor/zend/gdata/library'),
+    get_include_path(),
+)));
+
 require_once( __DIR__ . '/../../vendor/bolt/bolt/app/bootstrap.php' );
 
 $app['season'] = '2014-2015';
