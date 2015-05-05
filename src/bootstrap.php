2ef883e780430240b8f813f90c516b036bd8d492
Richard Hinkamp <richard@hinkamp.nl>
2015-05-05 12:52:43 +0200

    Obsolete file

--- src/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-set_include_path(implode(PATH_SEPARATOR, array(
-	realpath(__DIR__ . '/../vendor/zend/gdata/library'),
-	get_include_path(),
-)));
-
-require_once( __DIR__ . '/../app/src/bootstrap.php' );
