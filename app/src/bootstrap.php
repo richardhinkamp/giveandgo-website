65cdc3ac79d7344dd6b0acee53c030eb417ed21d
Richard Hinkamp <richard@hinkamp.nl>
2015-12-03 22:36:45 +0100

    Working with live config too

--- app/src/bootstrap.php
+++ app/src/bootstrap.php
@@ -2,9 +2,13 @@
 
 require_once(__DIR__ . "/../../vendor/autoload.php");
 $configuration = new Bolt\Configuration\Composer(__DIR__ . '/../..');
-$configuration->setPath("web", "web");
-$configuration->setPath("files", "web/files");
-$configuration->setPath("themebase", "web/theme");
+$dir = 'web';
+if( is_dir(__DIR__ . "/../../public_html") ) {
+    $dir = 'public_html';
+}
+$configuration->setPath("web", "$dir");
+$configuration->setPath("files", "$dir/files");
+$configuration->setPath("themebase", "$dir/theme");
 //$configuration->getVerifier()->disableApacheChecks();
 $configuration->verify();
 $app = new Bolt\Application(array('resources'=>$configuration));
